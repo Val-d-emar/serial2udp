e41085c9cf9c35953f9b4f40d45cbc0555445981
Val-d-emar <vhot@live.ru>
2023-08-14 23:47:30 +0400

    correct tests

--- serial2udpUnitTests.cc
+++ serial2udpUnitTests.cc
@@ -10,42 +10,42 @@
 TEST(TestParcing, BasicAssertions) {
   QByteArray in = "\n1234\n12345\n123456\n\n\n8\n";
   SerialSvc svc;
-  EXPECT_EQ( svc.parceIn(in), 4);
-  EXPECT_STREQ( svc.reads[0].toLocal8Bit(), "1234");
-  EXPECT_STREQ( svc.reads[1].toLocal8Bit(), "12345");
-  EXPECT_STREQ( svc.reads[2].toLocal8Bit(), "123456");
-  EXPECT_STREQ( svc.reads[3].toLocal8Bit(), "8");
+  ASSERT_EQ( svc.parceIn(in), 4);
+  ASSERT_STREQ( svc.reads[0].toLocal8Bit(), "1234");
+  ASSERT_STREQ( svc.reads[1].toLocal8Bit(), "12345");
+  ASSERT_STREQ( svc.reads[2].toLocal8Bit(), "123456");
+  ASSERT_STREQ( svc.reads[3].toLocal8Bit(), "8");
 }
 
 TEST(TestEncoding, BasicAssertions) {
   QString data = "12345678";
   QByteArray enc = Message(data).encrypt();
-  EXPECT_STREQ( enc, "HELLO23456789");
+  ASSERT_STREQ( enc, "HELLO23456789");
 }
 
-//TEST(TestBroadcasting, BasicAssertions) {
-//  int c = 1;
-//  char ** argv = new char*[1];
-//  QCoreApplication a(c, argv);
-//  SerialSvc svc(45454, "/dev/serial", &a);
-//  QString data = "12345678";
-//  emit svc.onCompleted(data);
-//  for (int i=0; i<=50; i++){
-//      a.processEvents();
-//      if (svc.datagram.size() > 0) {
-//          break;
-//      }
-//      usleep(100000);
-//  }
-//  EXPECT_STREQ( svc.datagram, "HELLO23456789");
-//}
+TEST(TestBroadcasting, BasicAssertions) {
+  int c = 1;
+  char ** argv = new char*[1];
+  QCoreApplication a(c, argv);
+  SerialSvc svc(45454, "/dev/serial", &a);
+  QString data = "12345678";
+  emit svc.onCompleted(data);
+  for (int i=0; i<=50; i++){
+      a.processEvents();
+      if (svc.datagram.size() > 0) {
+          break;
+      }
+      usleep(100000);
+  }
+  ASSERT_STREQ( svc.datagram, "HELLO23456789");
+}
 
 TEST(TestErrors, BasicAssertions) {
   char ** argv = new char*[3];
   int argc = 1;
-  EXPECT_EQ( ser2udp(argc, argv), 10);
+  ASSERT_EQ( ser2udp(argc, argv), 10);
   argc = 2;
-  EXPECT_EQ( ser2udp(argc, argv), 10);
+  ASSERT_EQ( ser2udp(argc, argv), 10);
   QString ar0("proga");
   QString ar1("/dev/wrongSerialPort");
   QString ar2("wrongUdpPort");
@@ -62,7 +62,7 @@ TEST(TestErrors, BasicAssertions) {
   strncpy(str, bar.data(), bar.size());
   argv[0] = str;
   argc = 3;
-  EXPECT_EQ( ser2udp(argc, argv), 20);
+  ASSERT_EQ( ser2udp(argc, argv), 20);
 //  ar2 = "64000";
 //  bar = ar2.toLatin1();
 //  str=(char *)malloc(bar.size()+1);
@@ -122,7 +122,7 @@ TEST(TestComm, BasicAssertions) {
         }
     }
     qDebug() << ls2;
-    EXPECT_LE( ls2.size(), 2);
+    ASSERT_LE( ls2.size(), 2);
 
     qDebug() << QString(ls2[0]);
     qDebug() << QString(ls2[1]);
@@ -143,6 +143,6 @@ TEST(TestComm, BasicAssertions) {
         }
         usleep(100000);
     }
-    EXPECT_STREQ( svc.datagram, "HELLO23456789");
+    ASSERT_STREQ( svc.datagram, "HELLO23456789");
     std::system(QString("pkill socat").toStdString().c_str());
 }
